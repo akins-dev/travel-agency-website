ae4c8127e5f6e59fb90dc469d5aad87551464ff7
akins_dev <bobemmanuel503@gmail.com>
2025-12-26 00:32:52 +0100

    refactor: clean up about us hero section

--- src/components/pages/about/Hero.tsx
+++ src/components/pages/about/Hero.tsx
@@ -6,16 +6,16 @@ export default function AboutHero() {
     <>
       <Navbar />
       <section className="bg-[#EAE8DF] w-full pt-32 pb-12 text-[#2B3D25] font-sans">
-        <div className="max-w-[1800px] mx-auto ">
+        <div className="max-w-450 mx-auto ">
           {/* Page Title */}
           <h1 className="text-[13vw] lg:text-[7.5rem] font-medium tracking-tight uppercase">
             About Us
           </h1>
 
           {/* Hero Image */}
-          <div className="relative w-fullh-[45vh] lg:h-[60vh] rounded-[2rem] overflow-hidden bg-[#E1DFD6]">
+          <div className="relative w-fullh-[45vh] lg:h-[60vh] rounded-4xl overflow-hidden bg-[#E1DFD6]">
             <Image
-              src="/images/acropolis.png" // Acropolis placeholder
+              src="/images/acropolis.png"
               alt="Acropolis of Athens"
               fill
               className="object-cover mix-blend-multiply contrast-[1.05]"
